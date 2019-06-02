4df4246ef5f6e0986a55199918ffb9cfcdbc61e3
Matthew Landauer <mlandauer@gmail.com>
2019-06-03 05:36:54 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -14,16 +14,7 @@ search_form = first_page.forms.first
 
 summary_page = agent.submit( search_form, search_form.buttons.first )
 
-data     = []
 page_num = 1
-
-# Only figure out the header stuff the first time...
-headers               = nil
-idx_council_reference = nil
-idx_description       = nil
-idx_date_received     = nil
-idx_address           = nil
-
 while summary_page
   table = summary_page.root.at_css('table.ContentPanel')
 
@@ -32,31 +23,17 @@ while summary_page
     next
   end
 
-  unless headers
-    headers = table.css('th').collect { |th| th.inner_text.strip }
-    # puts headers.inspect
-    # ["Our Reference", "Type of Application", "Date Lodged", "Location", "Details of application or permit", "Decision (where applicable)"]
-    idx_council_reference = headers.index("Our Reference")
-    idx_description       = headers.index('Type of Application')
-    idx_date_received     = headers.index('Date Lodged')
-    idx_address           = headers.index('Location')
-  end
-
-  data = table.css('.ContentPanel, .AlternateContentPanel').collect do |tr|
-    tr.css('td').collect { |td| td.inner_text.strip }
-  end
-
-  data.each do |application|
+  EpathwayScraper::Table.extract_table_data_and_urls(table).each do |row|
     info = {
-      'council_reference' => application[idx_council_reference],
-      'address' => application[idx_address],
-      'description' => application[idx_description],
+      'council_reference' => row[:content]["Our Reference"],
+      'address' => row[:content]["Location"],
+      'description' => row[:content]["Type of Application"],
       'info_url' => splash_url,
       'date_scraped' => Date.today.to_s
     }
 
-    if idx_date_received
-      info['date_received'] = Date.strptime(application[idx_date_received], '%d/%m/%Y').to_s
+    if row[:content]["Date Lodged"]
+      info['date_received'] = Date.strptime(row[:content]["Date Lodged"], '%d/%m/%Y').to_s
     end
 
     EpathwayScraper.save(info)
