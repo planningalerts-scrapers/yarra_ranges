7006482ad890fe20f59490906598244f85a9e68d
Matthew Landauer <mlandauer@gmail.com>
2019-06-03 06:26:04 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,6 @@
 require "epathway_scraper"
 
-base_url   = "https://epathway.yarraranges.vic.gov.au/ePathway/Production/Web"
-main_url   = "#{base_url}/GeneralEnquiry"
-splash_url = "#{base_url}/default.aspx"
-
-scraper = EpathwayScraper::Scraper.new(
-  "https://epathway.yarraranges.vic.gov.au/ePathway/Production"
+EpathwayScraper.scrape_and_save(
+  "https://epathway.yarraranges.vic.gov.au/ePathway/Production",
+  list_type: :all, max_pages: 20
 )
-
-splash_page = scraper.agent.get( scraper.base_url )
-
-first_page = scraper.agent.get( "#{main_url}/EnquiryLists.aspx" )
-
-search_form = first_page.forms.first
-
-summary_page = scraper.agent.submit( search_form, search_form.buttons.first )
-
-page_num = 1
-while summary_page
-  EpathwayScraper::Page::Index.scrape_index_page(summary_page, scraper.base_url, scraper.agent) do |record|
-    EpathwayScraper.save(record)
-  end
-
-  page_num = page_num + 1
-
-  # Fairly arbitrarily only read in the first 20 pages. Goes back roughly 6 months
-  break if page_num > 20
-
-  summary_page = scraper.agent.get( "#{main_url}/EnquirySummaryView.aspx", { :PageNumber => page_num } )
-end
