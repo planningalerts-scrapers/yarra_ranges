5b9b52069abee54f3483146b3940255652d2331d
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:12:14 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 EpathwayScraper.scrape_and_save(
   "https://epathway.yarraranges.vic.gov.au/ePathway/Production",
-  list_type: :all, max_pages: 20
+  list_type: :all, max_pages: 20, state: "VIC"
 )
